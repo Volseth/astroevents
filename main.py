d06d74d14f5722e0904b6e107371f4840a0210c6
Udek <b.duda11@wp.pl>
2021-04-11 13:18:58 +0200

    Updated example usage of method

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     unittest.TextTestRunner(verbosity=2).run(suite)
     longitude_warsaw = 21.01
     julian_date = 2459315.08333
-
-    print(find_corresponding_night(julian_date, longitude_warsaw))
+    print("Longitude: " + str(longitude_warsaw))
+    print("Julian date: " + str(julian_date))
+    print("Night of the event: " + find_corresponding_night(julian_date, longitude_warsaw))
 
